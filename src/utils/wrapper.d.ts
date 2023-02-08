72700c906301d292bacc3048296b7ca0a0398af4
Llydia Cross <llydiacross@gmail.com>
2023-02-08 22:02:43 +0000

    fixes

--- src/utils/wrapper.d.ts
+++ src/utils/wrapper.d.ts
@@ -1,9 +1,7 @@
-declare module "wrapper" {
-  export function loadPackages(): Promise<void>;
-  export function reloadPackages(): Promise<void>;
-  export function requireModule(nodeModule: string): Promise<any>;
-  export function reloadPackage(nodeModule: string): Promise<any>;
-  export function get(nodeModule: string): Promise<any>;
-  export function getSync(nodeModule: string): any;
-  export function isDebug(): boolean;
-}
+export function loadPackages(): Promise<void>;
+export function reloadPackages(): Promise<void>;
+export function requireModule(nodeModule: string): Promise<any>;
+export function reloadPackage(nodeModule: string): Promise<any>;
+export function get(nodeModule: string): Promise<any>;
+export function getSync(nodeModule: string): any;
+export function isDebug(): boolean;
